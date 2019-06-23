b76ae77bade30c685325a3ee226d063b281cd584
wangran1983 <wran01@163.com>
2019-06-23 22:13:19 +0800

    Initial commit

---
+++ src/main/java/com/donbala/controller/ShowIndexController.java
@@ -0,0 +1,13 @@
+package com.donbala.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShowIndexController {
+
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!helleeeo worlddddd222</h2>
+</body>
+</html>
